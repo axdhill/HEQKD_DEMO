6cdffcca8de3ffddcc65993ec7cbed90348b29d7
Alex <axdhill@gmail.com>
2018-09-25 17:47:27 -0500

    Deleted pointless files

--- HE_QKD_GUI.py
+++
@@ -1,6 +0,0 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
-from kivy.vector import Vector
-from kivy.clock import Clock
-from random import randint
